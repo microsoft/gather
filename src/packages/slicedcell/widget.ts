e3a890272faf64427a14990257e3669d6dfb1e33
Andrew Head <t-anhead@microsoft.com>
2018-07-09 10:32:08 -0700

    History: Show and hid unchanging text on click.

--- src/packages/slicedcell/widget.ts
+++ src/packages/slicedcell/widget.ts
@@ -31,6 +31,16 @@ const SLICED_CELL_HIGHLIGHTED_TEXT_CLASS = 'jp-SlicedCell-editor-highlightedtext
 const SLICED_CELL_BLURRED_TEXT_CLASS = 'jp-SlicedCell-editor-blurredtext';
 
 /**
+ * The class name added to editor text for hiding lines.
+ */
+const SLICED_CELL_HIDE_TEXT_CLASS = 'jp-SlicedCell-editor-hidetext';
+
+/**
+ * The class name to add to editor text for revealed lines.
+ */
+const SLICED_CELL_REVEAL_TEXT_CLASS = 'jp-SlicedEditor-editor-revealtext';
+
+/**
  * Number of lines of context to show before and after updated code.
  */
 const CONTEXT_SIZE = 1;
@@ -55,7 +65,10 @@ export class SlicedCell extends Widget {
 
         let layout = (this.layout = new PanelLayout());
         layout.addWidget(editor);
+        this.highlightCode();
+    }
 
+    highlightCode() {
         // XXX: Syntax highlighting only appears to work if we wait before applying it.
         // Though some other operations (e.g., marking text) without a delay.
         let codeMirrorEditor: CodeMirror.Editor = (this._editor.editor as CodeMirrorEditor).editor;
@@ -123,7 +136,7 @@ export class DiffedSlicedCell extends SlicedCell {
             // Build a list of lines that should be showing in the cell.
             let startPosition: CodeMirror.Position = codeMirrorDoc.posFromIndex(range.start);
             let endPosition: CodeMirror.Position = codeMirrorDoc.posFromIndex(range.end + 1);
-            for (let i = startPosition.line - CONTEXT_SIZE; i < endPosition.line + CONTEXT_SIZE; i++) {
+            for (let i = startPosition.line - CONTEXT_SIZE; i <= endPosition.line + CONTEXT_SIZE; i++) {
                 if (i < codeMirrorDoc.firstLine() || i > codeMirrorDoc.lastLine()) continue;
                 if (linesToShow.indexOf(i) == -1) {
                     linesToShow.push(i);
@@ -160,21 +173,51 @@ export class DiffedSlicedCell extends SlicedCell {
             hiddenLineRanges.push([hiddenRangeStart, codeMirrorDoc.lastLine() + 1]);
         }
         
-        // Hide the lines that should be hidden.
+        let revealMarkers: Array<CodeMirror.TextMarker> = new Array<CodeMirror.TextMarker>();
+        let editor: CodeMirror.Editor = (this._editor.editor as CodeMirrorEditor).editor;
+
+        // Hide all of the hidden lines.
+        let hideRange = function(from: CodeMirror.Position, to: CodeMirror.Position) {
+            let replacement: HTMLElement = document.createElement('span');
+            replacement.classList.add(SLICED_CELL_HIDE_TEXT_CLASS);
+            replacement.textContent = "... click to show hidden lines ...";
+            let hideMarker: CodeMirror.TextMarker = codeMirrorDoc.markText(from, to, { replacedWith: replacement });
+            // When someone clicks on a hidden line, reveal it.
+            replacement.onclick = function(event: MouseEvent) {
+                hideMarker.clear();
+                // If we don't refresh the editor, some of the hidden code might not get shown.
+                editor.refresh();
+                // Add a marker to the revealed code, so we can hide it again.
+                let revealMarker: CodeMirror.TextMarker = codeMirrorDoc.markText(
+                    from, to, { className: SLICED_CELL_REVEAL_TEXT_CLASS });
+                revealMarkers.push(revealMarker);
+                // This prevents the event by getting handled by the editor, which might detect
+                // this as a click on the `revealMarker` and hide the text again.
+                event.stopPropagation();
+            }
+        };
         hiddenLineRanges.forEach(function(lineRange: [number, number]) {
-            let replacement = document.createElement('span');
-            replacement.textContent = "...";
-            replacement.classList.add(SLICED_CELL_BLURRED_TEXT_CLASS);
-            codeMirrorDoc.markText(
-                { line: lineRange[0], ch: 0 },
-                { line: lineRange[1], ch: 0 },
-                { collapsed: true, replacedWith: replacement }
-            )
+            hideRange({ line: lineRange[0], ch: 0 }, { line: lineRange[1], ch: 0 });
         });
 
+        // Whenever someone clicks on hidden lines that were revealed, hide them again.
+        editor.getWrapperElement().onclick = function(event: MouseEvent) {
+            let clickPosition: CodeMirror.Position = editor.coordsChar({ left: event.clientX, top: event.clientY });
+            for (let marker of editor.getDoc().findMarksAt(clickPosition)) {
+                let markerIndex = revealMarkers.indexOf(marker);
+                if (markerIndex != -1) {
+                    let range = marker.find();
+                    marker.clear();
+                    revealMarkers.splice(markerIndex, 1);
+                    hideRange(range.from, range.to);
+                }
+            }
+        }
+
+        // TODO(andrewhead): set this as a configuration parameter.
         // If there is no new code in this cell, hide it.
         if (linesToShow.length == 0) {
-            this.hide();
+            // this.hide();
         }
     }
 }
